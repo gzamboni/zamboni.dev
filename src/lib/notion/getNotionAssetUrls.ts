20b16f7336c29c359ca84e146b62ff87a4d647cd
Giovani Zamboni <gzamboni@gmail.com>
2020-05-22 14:39:34 -0300

    fix: assets
    Signed-off-by: Giovani Zamboni <gzamboni@gmail.com>

--- src/lib/notion/getNotionAssetUrls.ts
+++ src/lib/notion/getNotionAssetUrls.ts
@@ -1,7 +1,6 @@
 import fetch from 'node-fetch'
 import { getError } from './rpc'
 import { NextApiResponse } from 'next'
-import { NOTION_TOKEN, API_ENDPOINT } from './server-constants'
 
 export default async function getNotionAsset(
   res: NextApiResponse,
@@ -10,6 +9,9 @@ export default async function getNotionAsset(
 ): Promise<{
   signedUrls: string[]
 }> {
+  const NOTION_TOKEN =
+    'df82cc2e5df3d6ed3d233dfcbed3c7d77e4f56f8300452720517bebdacd32c4a77b45c1393b6d889e6e8b4ac98a486f3c75cd44b1f953ace46c6190e77b0a585ecd4122077b324395307a603fadf'
+  const API_ENDPOINT = 'https://www.notion.so/api/v3'
   const requestURL = `${API_ENDPOINT}/getSignedFileUrls`
   const assetRes = await fetch(requestURL, {
     method: 'POST',
