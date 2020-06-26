f8b52360d7e25ca5a9a4239b726a00005e6485f8
Giovani Zamboni <gzamboni@gmail.com>
2020-06-26 19:59:26 -0300

    fix: assets

--- functions/assets.js
+++ functions/assets.js
@@ -1,13 +1,12 @@
-import querystring from 'querystring'
-import fetch from 'node-fetch'
+const fetch = require('node-fetch')
 
-exports.handler = function(event, context, callback) {
+exports.handler = async function(event, context, callback) {
   // your server-side functionality
   const API_ENDPOINT = 'https://www.notion.so/api/v3/getSignedFileUrls'
   const NOTION_TOKEN = process.env.NOTION_TOKEN
   const requestURL = API_ENDPOINT
-  const params = querystring.parse(event.body)
-  return fetch(requestURL, {
+  const params = event.queryStringParameters
+  const assetRes = await fetch(requestURL, {
     method: 'POST',
     headers: {
       cookie: 'token_v2=' + NOTION_TOKEN,
@@ -25,14 +24,64 @@ exports.handler = function(event, context, callback) {
       ],
     }),
   })
-    .then(response => ({
-      statusCode: 200,
-      body: response.json(),
-    }))
-    .catch(error => ({
-      statusCode: 422,
-      body: 'Oops! Something went wrong. ' + error,
-    }))
+  if (assetRes.ok) {
+    const jsonResponse = await assetRes.json()
+
+    callback(null, {
+      statusCode: 307,
+      headers: {
+        Location: jsonResponse.signedUrls.pop(),
+      },
+      body: '',
+    })
+  } else {
+    console.log('bad request', assetRes.status)
+    res.json({
+      status: 'error',
+      message: 'failed to load Notion asset',
+    })
+    throw new Error(await getError(assetRes))
+  }
+  // .then(response => response.json())
+  // .then(json => {
+  // 	callback(null, {
+  // 		statusCode: 200,
+  // 		body: json
+  // 	})
+  // })
+  // .catch(error => {
+  // 	callback(null, {
+  // 		statusCode: 422,
+  // 		body: 'Oops! Something went wrong. ' + error
+  // 	})
+  // })
+
+  //   return fetch(requestURL, {
+  //     method: 'POST',
+  //     headers: {
+  //       cookie: 'token_v2=' + NOTION_TOKEN,
+  //       'content-type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       urls: [
+  //         {
+  //           url: params.assetUrl,
+  //           permissionRecord: {
+  //             table: 'block',
+  //             id: params.blockId,
+  //           },
+  //         },
+  //       ],
+  //     }),
+  //   })
+  //     .then(response => ({
+  //       statusCode: 200,
+  //       body: response.json(),
+  //     }))
+  //     .catch(error => ({
+  //       statusCode: 422,
+  //       body: 'Oops! Something went wrong. ' + error,
+  //     }))
 
   // if (assetRes.ok) {
   // 	return assetRes.json()
