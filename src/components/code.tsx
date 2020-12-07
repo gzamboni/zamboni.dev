770e97072f8e086b9898b0fe2048e493a66b5e63
Giovani Zamboni <gzamboni@gmail.com>
2020-12-06 23:08:47 -0300

    chore: merge master
    Signed-off-by: Giovani Zamboni <gzamboni@gmail.com>

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -1,5 +1,6 @@
 import Prism from 'prismjs'
-import 'prismjs/components/prism-jsx'
+import 'prismjs/components/prism-python'
+import 'prismjs/components/prism-json'
 
 const Code = ({ children, language = 'javascript' }) => {
   return (
@@ -26,9 +27,25 @@ const Code = ({ children, language = 'javascript' }) => {
           display: block;
           padding: 0.8rem;
           line-height: 1.5;
-          background: #f5f5f5;
+          color: #f8f8f2;
+          background: #282a36;
           font-size: 0.75rem;
           border-radius: var(--radius);
+          font-family: Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace;
+          text-shadow: 0 1px rgba(0, 0, 0, 0.3);
+          text-align: left;
+          white-space: pre;
+          word-spacing: normal;
+          word-break: normal;
+          word-wrap: normal;
+          line-height: 1.5;
+          -moz-tab-size: 4;
+          -o-tab-size: 4;
+          tab-size: 4;
+          -webkit-hyphens: none;
+          -moz-hyphens: none;
+          -ms-hyphens: none;
+          hyphens: none;
         }
       `}</style>
     </>
